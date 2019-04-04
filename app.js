92c3315d120ad46fbbb60ec46d1bca31609f16f9
quonn-bernard <qber83@gmail.com>
2019-04-04 08:41:45 -0700

    added post functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //auth credss
 require('dotenv').config()
 
+//cors
+const cors = require('cors')
 // server variables
 const express = require('express');
 const app = express();
@@ -14,11 +16,17 @@ const moviedex = require('./moviedex.json')
 const morgan = require('morgan')
 app.use(morgan('dev'))
 
+//parses the body response 
+app.use(express.json());
+
+app.use(cors())
+app.use(express.static('public'));
+
 // validates client token against env token
 app.use(function validateBearerToken(req, res, next) {
 
     console.log('validate bearer token working as it should!')
-    
+
     // client token
     const bearerToken = req.query.token;
 
@@ -32,17 +40,17 @@ app.use(function validateBearerToken(req, res, next) {
 // handles API request results
 function handleGetMovies(req, res) {
     // creates request variables
-    const {genre,country,avg_vote} = req.query;
-    
+    const {genre, country, avg_vote} = req.query;
+
     // app movie data
     let movies = moviedex;
-    
+
     // filters results accorign to user input
     if (genre) {
         movies = movies.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
-    }else if(country){
+    } else if (country) {
         movies = movies.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
-    }else if(avg_vote){
+    } else if (avg_vote) {
         movies = movies.filter(movie => Number(movie.avg_vote) === Number(avg_vote));
     }
 
@@ -52,6 +60,14 @@ function handleGetMovies(req, res) {
 
 // moviedex API call
 app.get('/movie', handleGetMovies)
+
+function handlePostMovies(req, res) {
+    console.log(req.body);
+    res.send('POST Request received');
+  };
+
+// handles post requests
+app.post('/movie', handlePostMovies)
 
 // Port number
 const PORT = 8000
